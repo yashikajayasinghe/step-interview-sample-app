d2af3e2859d1dfa1a3619023eed344f838868a15
yashikajayasinghe <yash166@gmail.com>
2022-02-27 21:11:36 +1300

    validate title number field

--- web/src/components/TitlePage.js
+++ web/src/components/TitlePage.js
@@ -1,97 +1,124 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom"
-import { Table, Card, CardBody, Form, Input, Button } from "reactstrap";
+import React, {Component} from "react";
+import {withRouter} from "react-router-dom"
+import {Table, Card, CardBody, Form, Input, Button} from "reactstrap";
 
 class TitlePage extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        data: undefined,
-        ownerChangeValue: "",
-      }
-      this.loadTitle = this.loadTitle.bind(this);
-      this.ownerNameHandleChange = this.ownerNameHandleChange.bind(this);
-      this.ownerNameHandleSubmit = this.ownerNameHandleSubmit.bind(this);
+        super(props);
+        this.state = {
+            data: undefined,
+            ownerChangeValue: "",
+        }
+        this.loadTitle = this.loadTitle.bind(this);
+        this.ownerNameHandleChange = this.ownerNameHandleChange.bind(this);
+        this.ownerNameHandleSubmit = this.ownerNameHandleSubmit.bind(this);
     }
-    ownerNameHandleChange(event) {
-      this.setState({ownerChangeValue: event.target.value});
-    }
-    ownerNameHandleSubmit(event) {
-      event.preventDefault();
-      fetch(`/api/titles/${this.state.data.id}`, {
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ownerName: this.state.ownerChangeValue}),
-        })
-        .then(res => res.json())
-        .then(json => {
-          console.log(json)
-          this.setState({
-            data: json,
-            ownerChangeValue: '',
-          })
-        });
-    }
-    loadTitle() {
-      var titleNo = this.props.match.params.titleNo;
-      fetch(`/api/titles/${titleNo}`)
-        .then(res => res.json())
-        .then(json => {
-          console.log(json)
-          this.setState({
-            data: json,
-          })
-        });
-    }
-    componentDidMount() {
-      this.loadTitle();
-    }
-    componentDidUpdate(newProps) {
-      var titleNo = this.props.match.params.titleNo;
-      if(this.state.data && String(this.state.data.id) !== String(titleNo)) {
-        this.loadTitle();
-      }
-    }
-    render() {
-      var titleNo = this.props.match.params.titleNo;
-      var title = this.state.data;
-      return (
-        <div>
-          <h3>Title #{titleNo}</h3>
-          {this.state.data === undefined && <p>
-            Loading...
-          </p>}
-          {this.state.data && <div>
-            <Table>
-                <tbody>
-                    <tr>
-                        <th>Description</th>
-                        <td>{title.description}</td>
-                    </tr>
-                    <tr>
-                        <th>Current Owner</th>
-                        <td>{title.ownerName}</td>
-                    </tr>
-                </tbody>
-            </Table>
-            <Card color="light" style={{marginTop: "50px"}}>
-                <CardBody>
-                    <h4>Change Owner</h4>
-                    <p>As a registered conveyancing lawyer, you may record a change of ownership of this title.</p>
-                    <Form inline onSubmit={this.ownerNameHandleSubmit}>
-                        <Input type="text" value={this.state.ownerChangeValue} onChange={this.ownerNameHandleChange} 
-                            placeholder="Enter the new owner name" style={{width: "400px"}}/>
-                        &nbsp;
-                        <Button color="primary" type="submit" value="Submit">Save</Button>
-                    </Form>
-                </CardBody>
-            </Card>
 
-          </div>}
-        </div>
-      );
+    ownerNameHandleChange(event) {
+        this.setState({ownerChangeValue: event.target.value});
+    }
+
+    ownerNameHandleSubmit(event) {
+        event.preventDefault();
+        fetch(`/api/titles/${this.state.data.id}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ownerName: this.state.ownerChangeValue}),
+        })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                this.setState({
+                    data: json,
+                    ownerChangeValue: '',
+                })
+            });
+    }
+
+    loadTitle() {
+        if (this.props.match.params.titleNo) {
+            var titleNo = this.props.match.params.titleNo;
+
+            fetch(`/api/titles/${titleNo}`)
+                .then(res => {
+                    if (res.ok) {
+                        return res.json()
+                    } else {
+                        console.log(`response status is ${res.status}`)
+                        return Promise.reject('Undefined')
+                    }
+                })
+                .then(json => {
+                    console.log(json)
+                    this.setState({
+                        data: json,
+                    })
+                })
+                .catch(error => {
+                    this.setState({error: true});
+                    console.log("Title wasn't found. Error State..")
+                });
+        }
+
+    }
+
+    componentDidMount() {
+        this.loadTitle();
+    }
+
+    componentDidUpdate(newProps) {
+        var titleNo = this.props.match.params.titleNo;
+        if (this.state.data && String(this.state.data.id) !== String(titleNo)) {
+            this.loadTitle();
+        }
+    }
+
+    render() {
+        var titleNo = this.props.match.params.titleNo;
+        if (this.state.error) {
+            return <h1>Title Number {titleNo} Is Invalid.</h1>
+        }
+
+        var title = this.state.data;
+        return (
+            <div>
+                <h3>Title #{titleNo}</h3>
+                {this.state.data === undefined && <p>
+                    Loading...
+                </p>}
+                {this.state.data && <div>
+                    <Table>
+                        <tbody>
+                        <tr>
+                            <th>Description</th>
+                            <td>{title.description}</td>
+                        </tr>
+                        <tr>
+                            <th>Current Owner</th>
+                            <td>{title.ownerName}</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                    <Card color="light" style={{marginTop: "50px"}}>
+                        <CardBody>
+                            <h4>Change Owner</h4>
+                            <p>As a registered conveyancing lawyer, you may record a change of ownership of this
+                                title.</p>
+                            <Form inline onSubmit={this.ownerNameHandleSubmit}>
+                                <Input type="text" value={this.state.ownerChangeValue}
+                                       onChange={this.ownerNameHandleChange}
+                                       placeholder="Enter the new owner name" style={{width: "400px"}}/>
+                                &nbsp;
+                                <Button color="primary" type="submit" value="Submit">Save</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+
+                </div>}
+            </div>
+        );
     }
 }
 
--- web/src/components/TitleSearch.js
+++ web/src/components/TitleSearch.js
@@ -1,33 +1,46 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { Form, Input, Button } from "reactstrap";
+import React, {Component} from "react";
+import {withRouter} from "react-router-dom";
+import {Form, Input, Button} from "reactstrap";
+
+const rx_live = /^[+-]?\d*(?:[.,]\d*)?$/;
 
 class TitleSearch extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        value: '',
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = {
+            value: '',
+            validate: {}
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-    handleSubmit(event) {
-      this.props.history.push(`/titles/${this.state.value}`);
-    }
-    render() {
-      return (
-        <div style={{display: "inline-block"}}>
-          <Form inline onSubmit={this.handleSubmit}>
-            <Input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Enter a title number" />
-            &nbsp;
-            <Button color="primary" type="submit" value="Submit">Go</Button>
-          </Form>
-        </div>
-      );
-    }
-  }
 
-  export default withRouter(TitleSearch);
+    handleChange(event) {
+        if (rx_live.test(event.target.value)) {
+            this.setState({value: event.target.value});
+        }
+    }
+
+    handleSubmit(event) {
+        //Handle Empty form submition
+        if (this.state.value) {
+            this.props.history.push(`/titles/${this.state.value}`);
+        }
+
+    }
+
+    render() {
+        return (
+            <div style={{display: "inline-block"}}>
+                <Form inline onSubmit={this.handleSubmit}>
+                    <Input type="text" value={this.state.value} pattern="^[0-9]*$" onChange={this.handleChange}
+                           placeholder="Enter a title number"/>
+                    &nbsp;
+                    <Button color="primary" type="submit" value="Submit">Go</Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default withRouter(TitleSearch);
